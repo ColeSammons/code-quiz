340b34d469a344b27ff27e96e4b565a992767b1f
Cole Sammons <colemsammons@gmail.com>
2021-05-30 11:42:39 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var questions = [
+    {
+        quesions: "Which one of these is the correct way to comment out code in Javascript?",
+        answers: {
+            a: "//",
+            b: "<!-- -->",
+            c: "/* */"
+    },
+        correctAnswer: "a"
+    },
+
+]
